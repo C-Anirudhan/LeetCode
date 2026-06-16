3cb683ec7221074c85e2309b4745d9bd3f303fae
C-Anirudhan <cvbanirudhan@gmail.com>
2026-06-16 16:05:59 +0530

    Time: 50 ms (23.21%), Space: 19.3 MB (56.41%)

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -8,10 +8,6 @@ class Solution:
             ans += word2[j]
             i += 1
             j += 1
-        while i < len(word1):
-            ans += word1[i]
-            i += 1
-        while j < len(word2):
-            ans += word2[j]
-            j += 1
+        ans += word1[i:]
+        ans += word2[j:]
         return ans
